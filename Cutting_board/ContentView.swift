1ef9e28a436442b9e286f38edf6f400b293cae3d
WJIAEN <k123e456@icloud.com>
2026-02-02 21:42:38 +0800

    Initial Commit

---
+++ Cutting_board/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cutting_board
+//
+//  Created by WJIAEN on 2026/2/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cutting_board/Cutting_boardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cutting_boardApp.swift
+//  Cutting_board
+//
+//  Created by WJIAEN on 2026/2/2.
+//
+
+import SwiftUI
+
+@main
+struct Cutting_boardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
